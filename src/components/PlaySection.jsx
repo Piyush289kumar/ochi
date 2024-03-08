5cc353691372a56adc59673349b1a159b140d97f
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-08 11:22:48 +0530

    Playsection Animated

--- src/components/PlaySection.jsx
+++ src/components/PlaySection.jsx
@@ -13,8 +13,8 @@ function PlaySection() {
         });
     }, [rotate]);
     return (
-        <div className="w-full h-screen overflow-hidden">
-            <div className='relative w-full h-screen cursor-pointer bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")]'>
+        <div className="w-full h-screen overflow-hidden -z-[1] relative">
+            <div data-scroll data-scroll-section data-scroll-speed="-.6" className='relative w-full h-screen cursor-pointer bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")]'>
                 <div className="absolute top-[35%] left-1/2 -translate-x-[50%] -translate-y-[50%] flex gap-10">
                     <div className="bg-white h-[30vh] w-[30vh] rounded-full flex justify-center items-center gap-10">
                         <div className="relative bg-zinc-800 h-[58%] w-[58%] rounded-full ">
