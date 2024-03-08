4e55a94b5274a1caeb36387b5e339cba145528ef
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-08 10:21:35 +0530

    Init scroll.locomotive.ca

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import LandingPage from "./components/LandingPage";
 import { Navbar, Marquee, About, PlaySection, Features, Cards, Footer } from "./components/index";
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
 
+  const locomotiveScroll = new LocomotiveScroll();
+  
   return (
     <div className="w-full h-screen bg-zinc-100">
       <Navbar />
